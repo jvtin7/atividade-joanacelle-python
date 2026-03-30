e409de51a6e888fa145012e3fb63649159d92fa1
jvtin7 <silasjoao93@gmail.com>
2026-03-30 10:12:26 -0300

    Update questão g.py

--- questão g.py
+++ questão g.py
@@ -2,5 +2,5 @@
 horas_trabalhadas = float(input("Digite o número de horas trabalhadas: "))
 valor_hora = float(input("Digite o valor da hora trabalhada: "))
 salario_total = (vh * ht) 
-print ( "O valor do salario total é:", salario_total)) 
+print( "O valor do salario total é:", salario_total)) 
 
