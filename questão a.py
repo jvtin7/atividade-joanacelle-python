ca0382e014f180b41b6ff88c524829b3f5883478
jvtin7 <silasjoao93@gmail.com>
2026-03-31 08:36:06 -0300

    Refactor multiplication algorithm output format

--- questão a.py
+++ questão a.py
@@ -3,4 +3,4 @@
 num1 = int(input("Digite o primeiro número: "))
 num2 = int(input("Digite o segundo número: "))
 result = num1 * num2
-print(f"O resultado da multiplicação entre {num1} e {num2} é: {result}")
+print("O resultado da multiplicação de num1 e num2 é:", result)
